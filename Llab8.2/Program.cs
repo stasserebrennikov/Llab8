7fd8109cb533bda6b652d003e999729a7a0c5005
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-15 01:07:35 +0300

    Добавьте файлы проекта.

---
+++ Llab8.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Llab8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Lab_VIII.txt";
+            Random random = new Random();
+
+            using (StreamWriter sw = new StreamWriter(path))
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(0, 10));
+                }
+
+            int s = 0;
+            using (StreamReader sr = new StreamReader(path))
+                for (int i = 0; i < 10; i++)
+                {
+                    int x = Convert.ToInt32(sr.ReadLine());
+                    Console.Write($"{x} ");
+                    s += x;
+                }
+            Console.WriteLine();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Llab8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Llab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = "C:\\Users\\KK\\source\\repos\\Lab_7";
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }    
+}
